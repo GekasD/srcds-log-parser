b14617ba210f3244a0078f4ec2d2e38b6615db51
unknown <demetriusgekas@hotmail.com>
2024-08-09 22:09:36 +0300

    added flashbang entindex to 'threw' parser, with tests

--- src/parsers/threw.ts
+++ src/parsers/threw.ts
@@ -6,22 +6,30 @@ export type ThrewEventPayload = {
   player: Entity;
 
   item: string;
+
+  flashbangEntIndex?: number;
 };
 
 export type ThrewEvent = IBaseEvent<"threw", ThrewEventPayload>;
 
+// There is no typo in the flashbang entindex, there is just a random closing parenthesis for some reason
+
 // eslint-disable-next-line max-len
 // "PlayerName<93><STEAM_1:0:12345><CT>" threw molotov [-2035 1521 35]
+// "dimi<0><[U:1:221567857]><TERRORIST>" threw flashbang [-472 -336 11778] flashbang entindex 775)
 export const threwParser = defineParser<ThrewEvent>({
   type: "threw",
 
-  patterns: [concatPattern`^(?<player>${entityRe}) threw (?<item>.+) \\[(?<playerPosition>${vectorRe})\\]$`],
+  patterns: [
+    concatPattern`^(?<player>${entityRe}) threw (?<item>.+) \\[(?<playerPosition>${vectorRe})\\]$`,
+    concatPattern`^(?<player>${entityRe}) threw (?<item>.+) \\[(?<playerPosition>${vectorRe})\\] flashbang entindex (?<flashbangEntIndex>.+)\\)$`
+  ],
 
   parse({
     player,
     playerPosition,
-
     item,
+    flashbangEntIndex
   }) {
     return {
       player: {
@@ -29,7 +37,10 @@ export const threwParser = defineParser<ThrewEvent>({
 
         position: parseVector(playerPosition),
       },
+
       item,
+
+      flashbangEntIndex: Number(flashbangEntIndex) || undefined
     };
   },
 });
--- tests/threw.test.ts
+++ tests/threw.test.ts
@@ -1,32 +1,51 @@
 import { ok } from "assert";
 import { parse } from "../src";
 import { getEventString } from "./helpers/getEventString";
-import { counterTerroristTeam } from "./helpers/teams";
+import { counterTerroristTeam, terroristTeam } from "./helpers/teams";
 
 describe("threw", (): void => {
   it("should correctly parse", () => {
-    const log = getEventString('"PlayerName<93><[U:1:230970467]><CT>" threw molotov [-2035 1521 35]');
 
-    const result = parse(log);
+    const events: [string, Record<string, unknown>][] = [
+      [
+        '"PlayerName<93><[U:1:230970467]><CT>" threw molotov [-2035 1521 35]',
+        {
+          player: {
+            kind: "player",
+            entityId: 93,
+            steamId: "76561198191236195",
+            name: "PlayerName",
+            position: [-2035, 1521, 35],
+            team: counterTerroristTeam,
+          },
+    
+          item: "molotov",
+        }
+      ],
 
-    ok(result !== undefined, `Failed parse log: ${log}`);
+      [
+        '"dimi<0><[U:1:221567857]><TERRORIST>" threw flashbang [-472 -336 11778] flashbang entindex 775)',
+        {
+          player: {
+            kind: "player",
+            entityId: 0,
+            steamId: "76561198181833585",
+            name: "dimi",
+            position: [-472, -336, 11778],
+            team: terroristTeam
+          }
+        }
+      ]
+    ];
 
-    expect(result.type).toBe("threw");
-    expect(result.payload).toMatchObject({
-      player: {
-        kind: "player",
+    for (const [log, event] of events) {
+      const result = parse(getEventString(log));
 
-        entityId: 93,
-        steamId: "76561198191236195",
+      ok(result !== undefined, `Failed parse log: ${log}`);
 
-        name: "PlayerName",
+      expect(result.type).toBe("threw");
+      expect(result.payload).toMatchObject(event);
+    }
 
-        position: [-2035, 1521, 35],
-
-        team: counterTerroristTeam,
-      },
-
-      item: "molotov",
-    });
   });
 });
